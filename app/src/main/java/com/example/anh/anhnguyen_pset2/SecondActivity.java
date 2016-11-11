299d08517505ab7cec292eb2cfc07bd285d23bde
Your Name <example@example.com>
2016-11-11 22:42:47 +0100

    tijdelijk

--- app/src/main/java/com/example/anh/anhnguyen_pset2/SecondActivity.java
+++ app/src/main/java/com/example/anh/anhnguyen_pset2/SecondActivity.java
@@ -1,20 +1,106 @@
+
 package com.example.anh.anhnguyen_pset2;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.*;
+import java.util.*;
+
+import static com.example.anh.anhnguyen_pset2.R.id.okbutton;
+
 
 public class SecondActivity extends AppCompatActivity {
 
+Story story;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button okbutton;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+
+
+       ;
+
+        try {
+            inputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("TESTT", "hallo");
+
+        userInput();
+
     }
 
-    public void sendMessage (View view){
-        Intent startNewActivity = new Intent(this, ThirdActivity.class);
-        startActivity(startNewActivity);
+    public void inputStream()
+            throws IOException {
+        InputStream targetStream = getResources().openRawResource(R.raw.madlib1_tarzan);
+        story = new Story(targetStream);
+        Log.d("Hoi", String.valueOf(story.getPlaceholderCount()));
+
     }
+
+  public void userInput(){
+        int placecount = story.getPlaceholderCount();
+        String typeofword = story.getNextPlaceholder();
+        String hint = story.getNextPlaceholder();
+        ((EditText) findViewById(R.id.editText)).setHint(hint);
+        ((TextView) findViewById(R.id.typetext)).setText("Please fill in a/an " + typeofword );
+        ((TextView) findViewById(R.id.placeCount)).setText(placecount + " word / words left" );
+        }
+
+    public void addWord(View view){
+        String replaced=story.getNextPlaceholder();
+        int placecount = story.getPlaceholderRemainingCount();
+        String typeofword = story.getNextPlaceholder();
+        String hint = story.getNextPlaceholder();
+        Toast.makeText(getBaseContext(),"hey",Toast.LENGTH_LONG).show();
+        ((EditText) findViewById(R.id.editText)).setHint(hint);
+        ((TextView) findViewById(R.id.typetext)).setText("Please fill in a/an " + typeofword );
+
+        story.getNextPlaceholder();
+        story.fillInPlaceholder(replaced);
+
+        ((TextView) findViewById(R.id.editText)).setText("");
+        ((TextView) findViewById(R.id.placeCount)).setText(placecount + " word / words left" );
+
+
+
+
+
+
+            if (story.isFilledIn() == true){
+                Log.d("jamal", story.toString());
+                Intent i = new Intent(this, ThirdActivity.class);
+
+                startActivity(i);;
+            }
+        }
+
+
+
+    
+
+
+
 }
+
+
+
+
+
+
+
--- app/src/main/java/com/example/anh/anhnguyen_pset2/ThirdActivity.java
+++ app/src/main/java/com/example/anh/anhnguyen_pset2/ThirdActivity.java
@@ -1,13 +1,73 @@
 package com.example.anh.anhnguyen_pset2;
 
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 
 public class ThirdActivity extends AppCompatActivity {
+
+   //Button b_read;
+   TextView parsetext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        parsetext = (TextView) findViewById(R.id.parsetext);
+        String text = "";
+        try{
+            InputStream is = getAssets().open("madlib1_tarzan.txt");
+            int size =  is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            text= new String(buffer);
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+        parsetext.setText(text);
+
     }
-}
+
+        /*b_read = (Button) findViewById(R.id.b_read);
+
+        parsetext = (TextView) findViewById(R.id.parsetext);
+
+        b_read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String text = "";
+                try{
+                    InputStream is = getAssets().open("madlib0_simple.txt");
+                    int size =  is.available();
+                    byte[] buffer = new byte[size];
+                    is.read(buffer);
+                    is.close();
+                    text= new String(buffer);
+                    }
+                catch (IOException ex){
+                    ex.printStackTrace();
+                }
+                parsetext.setText(text);
+
+            }
+        });*/
+    }
+
+
+
